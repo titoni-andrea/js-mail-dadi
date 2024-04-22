df7402ce1311e5d346b53d231663aabaeda9997f
titoni-andrea <andreatitoni@gmail.com>
2024-04-22 17:34:53 +0200

    BONUSSS!!!

--- mail/js/main.js
+++ mail/js/main.js
@@ -2,8 +2,21 @@ let mailList = ["mail1@mail.it", "mail2@mail.it", "mail3@mail.it", "mail4@mail.i
 
 let mailUser = prompt("Scrivi la tua mail per accedere")
 
+let verificaAccesso = false
+
 for (i = 0; i < mailList.length; i++) {
     if (mailList[i] == mailUser) {
-        console.log("Puoi accedere")
+        verificaAccesso = true
     }
+    else {
+
+    }
+}
+
+if (verificaAccesso == true) {
+    console.log("puoi accedere")
+}
+
+else {
+    console.log("non puoi accedere")
 }
