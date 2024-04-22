28beed83a87232b0715c34d4acf1c733e7b0fc35
titoni-andrea <andreatitoni@gmail.com>
2024-04-22 15:46:45 +0200

    tolti i console.log

--- dadi/js/main.js
+++ dadi/js/main.js
@@ -1,9 +1,6 @@
 let numberUser = Math.floor(Math.random() * 6 + 1)
 let numberComputer = Math.floor(Math.random() * 6 + 1)
 
-console.log(numberComputer)
-console.log(numberUser)
-
 if (numberComputer < numberUser) {
     alert("HAI VINTO!")
 }
